78d5417831aeab397e854280c437623f8d12e5e3
micronn <nanami_tanida@rebuild-inc.co.jp>
2024-09-15 17:36:46 +0900

    [update] 不要なコード削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { SampleComponent } from '@/app/_components/sample-component'
-import { createServerComponentClient } from '@/utils/supabase/server'
 
 export default async function Notes() {
-  const supabase = createServerComponentClient()
-  const { data: notes } = await supabase.from('notes').select()
-
   return <SampleComponent />
 }
